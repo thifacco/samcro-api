c2f470c4d960a7fa0e200b7eb6665cccab181ff2
Tiago Facco <thifacco@gmail.com>
2021-09-12 20:59:24 -0300

    CRUD

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -26,18 +26,33 @@ export class TaskService {
 
   getById(id: number) {
     const task = this.tasks.find(task => task.id == id);
-    return task;  
+    return task;
   }
 
   create(task: Task) {
+    let lastId = 0;
+    if (this.tasks.length > 0) {
+      lastId = this.tasks[this.tasks.length - 1].id;
+    }
 
+    task.id = lastId + 1;
+    this.tasks.push(task);
+
+    return task;
   }
 
   update(task: Task) {
+    const taskArray = this.getById(task.id);
+    if (taskArray) {
+      taskArray.description = task.description;
+      taskArray.completed = task.completed;
+    }
 
+    return taskArray;
   }
 
   delete(id: number) {
-
+    const index = this.tasks.findIndex(task => task.id == id);
+    this.tasks.splice(index, 1);
   }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { identity } from 'rxjs';
 import { Task } from './shared/task';
 import { TaskService } from './shared/task.service';
 
@@ -16,6 +17,22 @@ export class TasksController {
 
    @Get(':id')
    async getById(@Param('id') id: number): Promise<Task> {
-      return this.taskService.getById(id); 
+      return this.taskService.getById(id);
+   }
+
+   @Post()
+   async create(@Body() task: Task): Promise<Task> {
+      return this.taskService.create(task);
+   }
+
+   @Put(':id')
+   async update(@Param('id') id: number, @Body() task: Task): Promise<Task> {
+      task.id = id;
+      return this.taskService.update(task);
+   }
+
+   @Delete(':id')
+   async delete(@Param('id') id: number) {
+      this.taskService.delete(id);
    }
 }
