9a6ede587717cca32bf48fb97eff206d1d3f0187
Tiago Facco <thifacco@gmail.com>
2021-09-12 23:27:06 -0300

    Strings obsoletas

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-
-// Z0gXTofUUtvqb98L
-// mongodb://samcrodev:Z0gXTofUUtvqb98L@samcroapidev.ceka4.mongodb.net/samcro?retryWrites=true&w=majority
-// mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
-
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/samcroapi'),
